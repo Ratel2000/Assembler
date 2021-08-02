2afbd023f321bebf759fe839d5a2b29aab7a67c5
Ayala122 <72230193+Ayala122@users.noreply.github.com>
2021-08-02 17:03:09 +0300

    Update dataImage.h

--- dataImage.h
+++ dataImage.h
@@ -1,10 +1,28 @@
 #ifndef DATA_IMAGE_H
 #define DATA_IMAGE_H
+
+/* ~~~~~~~~~~~~~~~~~~~~~~ MSGS ~~~~~~~~~~~~~~~~~~~~~~ */
+
+char* MALLOC_FAILD = "Malloc Failed - Cannot Build List!";
+
+
+/* ~~~~~~~~~~~~~~~~~~~~ Includes ~~~~~~~~~~~~~~~~~~~~ */
+
 #include "defaults.h"
 
-void addWordToImage(wordNodePtr*,Word,unsigned long);
-void freeList(wordNodePtr*);
-int getDestAdd(wordNodePtr,int);
-int getOpcode(wordNodePtr,int);
-int getSrcAdd(wordNodePtr,int);
+
+/*  ~~~~~~~~~~~~~~ Methods Declaration ~~~~~~~~~~~~~~ */
+
+void addWordToImage(wordNodePtr *, Word, unsigned long);
+
+void freeList(wordNodePtr *);    /* free the space */
+
+int getDestAdd(wordNodePtr, int);
+
+int getOpcode(wordNodePtr, int);
+
+int getSrcAdd(wordNodePtr, int);
+
+
+
 #endif
