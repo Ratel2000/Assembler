11d0762199042345578ec7e05eb29ffdde4a938e
Ayala122 <72230193+Ayala122@users.noreply.github.com>
2021-08-02 16:58:42 +0300

    Update labelTableLinkedList.h

--- labelTableLinkedList.h
+++ labelTableLinkedList.h
@@ -1,11 +1,30 @@
 #ifndef LABEL_TABLE_LINKED_LIST_H
 #define LABEL_TABLE_LINKED_LIST_H
+
+/* ~~~~~~~~~~~~~~~~~~~~~~ MSGS ~~~~~~~~~~~~~~~~~~~~~~ */
+
+char* CANNOT_BUILD = "Cannot Build List!";
+
+
+/* ~~~~~~~~~~~~~~~~~~~~ Includes ~~~~~~~~~~~~~~~~~~~~ */
+
 #include "defaults.h"
 
-void addToList(labelTableNodePtr*,labelTableNode);
-void freeLabelList(labelTableNodePtr*);
-Bool labelInList(labelTableNodePtr,char*);
-Type getLabelType(labelTableNodePtr,char*);
-int getLabelAddress(labelTableNodePtr,char*);
-void setType(labelTableNodePtr,char*,Type);
+
+/*  ~~~~~~~~~~~~~~ Methods Declaration ~~~~~~~~~~~~~~ */
+
+void setType(labelTableNodePtr, char *, Type);
+
+void addToList(labelTableNodePtr *, labelTableNode);
+
+void freeLabelList(labelTableNodePtr *);    /* free the space */
+
+Bool labelInList(labelTableNodePtr, char *);
+
+Type getLabelType(labelTableNodePtr, char *);
+
+int getLabelAddress(labelTableNodePtr, char *);
+
+
+
 #endif
